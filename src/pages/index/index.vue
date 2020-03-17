418bbe5fb05aa1069cef578f3f4fcbb2c6c7e215
harold1024 <744924498@qq.com>
2020-03-17 16:07:51 +0800

    Signed-off-by: harold1024 <744924498@qq.com>

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <input class="search" type="text" placeholder="请输入关键字搜索该品牌主题" v-model="name" maxlength="6">
+    <!-- <input class="search" type="text" placeholder="请输入关键字搜索该品牌主题" v-model="name" maxlength="6">
     <van-button type="primary">按钮</van-button>
+    <van-cell-group>
+    <van-field
+      :value="value"
+      placeholder="请输入用户名"
+      :border="false"
+      bind:change="onChange"
+    />
+    </van-cell-group> -->
+    <van-search placeholder="请输入搜索关键词" :value="value1" background="#4fc08d"/>
   </div>
 </template>
 
@@ -11,6 +20,8 @@ export default {
   data () {
     return {
       // name:'请输入关键字搜索该品牌主题',
+      value: '',
+      value1: ''
     }
   },
 
@@ -18,6 +29,10 @@ export default {
   },
 
   methods: {
+    onChange(event) {
+      // event.detail 为当前输入的值
+      console.log(event.detail);
+    }
   },
 
   created () {
